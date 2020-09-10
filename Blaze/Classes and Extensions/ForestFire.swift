dfcfb7c8f3f58e4a431fee174bfa8b12984e3e64
b0kch01 <nath49ers@gmail.com>
2020-09-09 19:09:52 -0700

    Migration to new dataset

--- Blaze/Classes and Extensions/ForestFire.swift
+++ Blaze/Classes and Extensions/ForestFire.swift
@@ -8,85 +8,189 @@
 import Foundation
 import MapKit
 
+// MARK: - JSON Wrapper Object
 
-/// A data structure that represents a forest fire
-struct ForestFire: Codable, Comparable, Identifiable {
-    // MARK: - Comparable Protocol Functions
+struct Incidents: Codable {
+    let incidents: [ForestFire]
+    let allAcres: Int
     
-    static func < (lhs: ForestFire, rhs: ForestFire) -> Bool {
-        return lhs.updated < rhs.updated
+    enum CodingKeys: String, CodingKey {
+        case incidents = "Incidents"
+        case allAcres = "AllAcres"
+    }
+}
+
+// MARK: - Incident Struct
+
+struct ForestFire: Codable, Identifiable {
+
+    static func name(lhs: ForestFire, rhs: ForestFire) -> Bool {
+        return lhs.name > rhs.name
     }
     
-    static func > (lhs: ForestFire, rhs: ForestFire) -> Bool {
-        return lhs.updated > rhs.updated
+    static func dateUpdated(lhs: ForestFire, rhs: ForestFire) -> Bool {
+        return lhs.updated > lhs.updated
     }
     
-    // MARK: - Attributes
+    let id = UUID()
     
-    var id = UUID()
-    var name: String /// name of the fire
-    var updated: Date /// `Date` of when the data was last updated
-    var start: Date /// `Date` of when the fire was first documented
-    var county: String /// counties affected by the fire
-    var location: String /// location(s) of the fire
-    var acres: Double /// area in acres of the fire
-    var contained: Double /// percent contained
-    var longitude: Double /// estimated starting location (longitude)
-    var latitude: Double /// estimated starting location (latitude)
-    var url: String /// url of the fire for more information at https://fire.ca.gov/incidents
-    
-    // MARK: - Init
-    init(name: String? = "Some Fire", updated: Date? = Date(), start: Date? = Date(), county: String? = "Some county", location: String? = "Some location", acres: Double? = 0, contained: Double? = 0, longitude: Double? = 36, latitude: Double? = 110, url: String? = "https://firenotfound") {
-        self.name = name!
-        self.updated = updated!
-        self.start = start!
-        self.county = county!
-        self.location = location!
-        self.acres = acres!
-        self.contained = contained!
-        self.longitude = longitude!
-        self.latitude = latitude!
-        self.url = url!
-    }
-    
-    // MARK: - CodingKeys
-    
-    /// Specific for `https://fire.ca.gov/ API`
+    let name: String
+    let location: String
+    let latitude: Double
+    let longitude: Double
+    let acresO: Int?
+    let containedO: Int?
+    let controlStatement: String?
+    let conditionStatement: String?
+    let counties: [String]
+    let countyIDS: String
+    let searchDescription: String?
+    let searchKeywords: String?
+    let adminUnit: String?
+    let updated: Date
+    let started: Date
+    let archiveYear: Int
+    let incidentPublic: Bool
+    let active: Bool
+    let featured: Bool
+    let calFireIncident: Bool
+    let majorIncident: Bool
+    let incidentFinal: Bool
+    let status: String
+    let url: String
+    let structuresDestroyed: Int?
+    let structuresDamaged: Int?
+    let structuresThreatened: Int?
+    let personnelInvolved: Int?
+    let crewsInvolved: Int?
+    let injuries: Int?
+    let fatalities: Int?
+    let helicopters: Int?
+    let engines: Int?
+    let dozers: Int?
+    let waterTenders: Int?
+    let airTankers: Int?
+
     enum CodingKeys: String, CodingKey {
         case name = "Name"
-        case updated = "Updated"
-        case start = "Started"
-        case county = "County"
         case location = "Location"
-        case acres = "AcresBurned"
-        case contained = "PercentContained"
-        case longitude = "Longitude"
         case latitude = "Latitude"
-        case url = "Url"
+        case longitude = "Longitude"
+        case acresO = "AcresBurned"
+        case containedO = "PercentContained"
+        case controlStatement = "ControlStatement"
+        case conditionStatement = "ConditionStatement"
+        case counties = "Counties"
+        case countyIDS = "CountyIds"
+        case searchDescription = "SearchDescription"
+        case searchKeywords = "SearchKeywords"
+        case adminUnit = "AdminUnit"
+        case updated = "Updated"
+        case started = "Started"
+        case archiveYear = "ArchiveYear"
+        case incidentPublic = "Public"
+        case active = "Active"
+        case featured = "Featured"
+        case calFireIncident = "CalFireIncident"
+        case majorIncident = "MajorIncident"
+        case incidentFinal = "Final"
+        case status = "Status"
+        case url = "CanonicalUrl"
+        case structuresDestroyed = "StructuresDestroyed"
+        case structuresDamaged = "StructuresDamaged"
+        case structuresThreatened = "StructuresThreatened"
+        case personnelInvolved = "PersonnelInvolved"
+        case crewsInvolved = "CrewsInvolved"
+        case injuries = "Injuries"
+        case fatalities = "Fatalities"
+        case helicopters = "Helicopters"
+        case engines = "Engines"
+        case dozers = "Dozers"
+        case waterTenders = "WaterTenders"
+        case airTankers = "AirTankers"
+    }
+    
+    init(
+        name: String?=nil,
+        updated: Date?=nil,
+        started: Date?=nil,
+        location: String?=nil,
+        counties: [String]?=nil,
+        latitude: Double?=nil,
+        longitude: Double?=nil,
+        acres: Int?=nil,
+        contained: Int?=nil,
+        url: String?=nil
+    ) {
+        self.name = name ?? "Wildfire"
+        self.location = location ?? "California"
+        self.latitude = latitude ?? 31.7
+        self.longitude = longitude ?? -122.3
+        self.acresO = acres ?? 0
+        self.containedO = contained ?? 0
+        
+        self.controlStatement = ""
+        self.conditionStatement = ""
+        self.counties = [""]
+        self.countyIDS = ""
+        self.searchDescription = ""
+        self.searchKeywords = ""
+        self.adminUnit = ""
+        self.updated = updated ?? Date()
+        self.started = started ?? Date()
+        self.archiveYear = 2020
+        self.incidentPublic = true
+        self.active = true
+        self.featured = false
+        self.calFireIncident = true
+        self.majorIncident = true
+        self.incidentFinal = false
+        self.status = "Active"
+        self.url = url ?? "/incidents"
+        self.structuresDestroyed = -1
+        self.structuresDamaged = -1
+        self.structuresThreatened = -1
+        self.personnelInvolved = -1
+        self.crewsInvolved = -1
+        self.injuries = -1
+        self.fatalities = -1
+        self.helicopters = -1
+        self.engines = -1
+        self.dozers = -1
+        self.waterTenders = -1
+        self.airTankers = -1
     }
     
     // MARK: - String Functions
     
     /// Returns a formatted string of the percent contained
     func getContained() -> String {
-        return contained != 0 ? "\(contained)%" : "Unknown"
+        return contained != -1 ? "\(contained)%" : "Unknown"
     }
-    
+
     /// Returns a formatted string (with commas) of acres
     func getAreaString() -> String {
-        return acres != 0 ? "\(acres.inCommas() ?? String(acres)) Acres" : "Unknown Area"
+        return acres != -1 ? "\(acres.inCommas() ?? String(acres)) Acres" : "Unknown Area"
     }
-    
+
     /// Returns a formatted string (with proper responses) for the location
     func getLocation() -> String {
         if location.lowercased() == "see details below" {
             return "More details on the website"
         }
-        
+
         return location.prefix(1).capitalized + location.dropFirst()
     }
-    
+
     // MARK: - Computed Properties
+
+    var acres: Int {
+        get { self.acresO ?? -1 }
+    }
+    
+    var contained: Int {
+        get { self.containedO ?? -1 }
+    }
     
     /// Computes a `CLLocationCoordinate2D` from the latitude and longitude attribute
     var coordinate: CLLocationCoordinate2D {
