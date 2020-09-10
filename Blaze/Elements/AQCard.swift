afc13a7347a3bf807079ab7591bdc9feebfade62
Polarizz <polarizz39@gmail.com>
2020-09-09 19:12:14 -0700

    Commit to catch up

---
+++ Blaze/Elements/AQCard.swift
@@ -0,0 +1,20 @@
+//
+//  AQCard.swift
+//  Blaze
+//
+//  Created by Paul Wong on 9/9/20.
+//
+
+import SwiftUI
+
+struct AQCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AQCard_Previews: PreviewProvider {
+    static var previews: some View {
+        AQCard()
+    }
+}
