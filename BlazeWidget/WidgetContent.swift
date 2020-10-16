00eaa74834575cad85c06bb1a98cdfbd3d638f3b
Max Kerns <60684506+Sakend@users.noreply.github.com>
2020-10-15 19:14:26 -0700

    some files didnt push

---
+++ BlazeWidget/WidgetContent.swift
@@ -0,0 +1,14 @@
+//
+//  WidgetContent.swift
+//  Blaze
+//
+//  Created by Max on 10/15/20.
+//
+
+import WidgetKit
+
+struct WidgetContent: TimelineEntry {
+  var date = Date()
+  let number: String
+  
+}
